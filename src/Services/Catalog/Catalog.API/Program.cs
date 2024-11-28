bd928bff60344fc7320210aa0006d593d54c96e9
Gavin Park <gpark@hotmail.com>
2024-11-28 16:00:10 +0000

    catalogapi

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to container
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+//Configure HTTP request pipeline
+app.MapCarter();
+
+app.Run();
